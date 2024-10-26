7770ebe18faa18caa3d134b1abdcc1593b0ac90a
haqproject <38024884+0xishaq@users.noreply.github.com>
2024-10-26 10:35:43 +0700

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -1,13 +1,47 @@
 echo -e "\e[1;32m                
-     __      __        _______       ____________
-    |  |    |  |      |  ___  |     |   _____   |
-    |  |____|  |     |  |___|  |    |  |     |  |
-    |  ______  |    |   _____   |   |  |     |  |
-    |  |    |  |   |  |       |  |  |  |_____|  |
-    |__|    |__|  |__|         |__| |_________\ \
-                                               \_\
-    
- ⋆ ——————————————————————————————————————————————————— ⋆
-   X : https://x.com/0xheyishaq ▪️ TG : https://t.me/haqproject
- ⋆ ——————————————————————————————————————————————————— ⋆
+
 \e[0m"
+
+
+#!/bin/bash
+
+check_and_install_pv() {
+    if ! command -v pv &> /dev/null; then
+        sudo apt update -y > /dev/null 2>&1
+        sudo apt install pv -y > /dev/null 2>&1
+    fi
+}
+
+print() {
+    colors=("\e[1;31m" "\e[1;32m" "\e[1;33m" "\e[1;34m" "\e[1;35m" "\e[1;36m" "\e[1;37m")
+
+    for line in "$@"; do
+        color=${colors[RANDOM % ${#colors[@]}]}
+        echo -e "${color}${line}" | pv -qL 100
+        sleep 0.2
+    done
+}
+
+
+reset='\e[0m'
+
+check_and_install_pv
+
+logo_lines=(
+"    __      __        _______       ____________"
+"   |  |    |  |      |  ___  |     |   _____   |"
+"   |  |____|  |     |  |___|  |    |  |     |  |"
+"   |  ______  |    |   _____   |   |  |     |  |"
+"   |  |    |  |   |  |       |  |  |  |_____|  |"
+"   |__|    |__|  |__|         |__| |_________\ \"
+"                                              \_\"
+""
+"⋆ ——————————————————————————————————————————————————— ⋆"
+"  X : https://x.com/0xheyishaq ▪️ TG : https://t.me/haqproject"
+"⋆ ——————————————————————————————————————————————————— ⋆"
+)
+
+
+echo -e "$reset"
+print "${logo_lines[@]}"
+echo -e "$reset"
